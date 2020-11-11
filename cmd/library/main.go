628dc72089c25d3e94efaefca72750b8297fcdd3
zrecovery <zrecovery@outlook.com>
2020-11-11 09:24:27 +0800

    remove template

--- cmd/library/main.go
+++ cmd/library/main.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"crypto/subtle"
-	"html/template"
-	"io"
 
 	articleService "github.com/zrecovery/library/pkg/article/service"
 
@@ -14,14 +12,6 @@ import (
 	_ "github.com/lib/pq"
 )
 
-type Template struct {
-	templates *template.Template
-}
-
-func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
-	return t.templates.ExecuteTemplate(w, name, data)
-}
-
 func main() {
 	e := echo.New()
 
@@ -30,12 +20,6 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-
-	t := &Template{
-		templates: template.Must(template.ParseGlob("web/views/*.html")),
-	}
-
-	e.Renderer = t
 
 	articleMod := articleService.NewArticleModule(db)
 
