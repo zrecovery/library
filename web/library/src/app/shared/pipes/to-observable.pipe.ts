bfbe5fa24bb784a930d32ccb65cc5fbab996f8d6
zrecovery <zrecovery@outlook.com>
2020-11-18 12:53:21 +0800

    移除前端页面

--- web/library/src/app/shared/pipes/to-observable.pipe.ts
+++
@@ -1,9 +0,0 @@
-import { Pipe, PipeTransform } from '@angular/core';
-import { Observable, of, isObservable } from 'rxjs';
-
-@Pipe({ name: 'toObservable' })
-export class ToObservablePipe implements PipeTransform {
-  transform(value: string | Observable<any>): Observable<string> {
-    return isObservable(value) ? value : of(value);
-  }
-}
--- web/library/src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  SERVER_ORIGIN: '',
-  production: true,
-  useHash: true,
-  hmr: false,
-};
