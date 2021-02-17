bd2469a8900d861331a9c1e951aa78c3f332352e
zrecovery <zrecovery@outlook.com>
2021-02-17 09:51:39 +0800

    优化测试数据库配置

--- internal/article/internal/repository/postgres_test.go
+++ internal/article/internal/repository/postgres_test.go
@@ -7,26 +7,30 @@ import (
 	"context"
 	"database/sql"
 	"log"
+	"os"
 	"testing"
 
 	_ "github.com/lib/pq"
 	"github.com/zrecovery/library/internal/article/pkg/article"
 )
 
-//
-func newTestPostgresql() *sql.DB {
+// 测试数据库
+var field struct {
+	DB *sql.DB
+}
+
+func TestMain(m *testing.M) {
 	db, err := sql.Open("postgres", "postgres://postgres:postgresd@localhost/library_test?sslmode=disable")
 	if err != nil {
 		log.Fatal(err)
 	}
-	return db
+	field.DB = db
+	r := m.Run()
+	field.DB.Close()
+	os.Exit(r)
 }
 
-//
 func TestPostgresRepository_saveBook(t *testing.T) {
-	type fields struct {
-		DB *sql.DB
-	}
 	type args struct {
 		ctx    context.Context
 		book   string
@@ -34,16 +38,25 @@ func TestPostgresRepository_saveBook(t *testing.T) {
 	}
 	tests := []struct {
 		name    string
-		fields  fields
+		db      *sql.DB
 		args    args
 		wantErr bool
 	}{
-		// TODO: Add test cases.
+		{
+			name: "文章模块中保存书籍",
+			db:   field.DB,
+			args: args{
+				ctx:    context.Background(),
+				book:   "test book",
+				author: "test author",
+			},
+			wantErr: false,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			r := &PostgresRepository{
-				DB: tt.fields.DB,
+				DB: tt.db,
 			}
 			if err := r.saveBook(tt.args.ctx, tt.args.book, tt.args.author); (err != nil) != tt.wantErr {
 				t.Errorf("PostgresRepository.saveBook() error = %v, wantErr %v", err, tt.wantErr)
@@ -53,25 +66,20 @@ func TestPostgresRepository_saveBook(t *testing.T) {
 }
 
 func TestPostgresRepository_Insert(t *testing.T) {
-	type fields struct {
-		DB *sql.DB
-	}
 	type args struct {
 		ctx context.Context
 		a   *article.Article
 	}
 	tests := []struct {
 		name    string
-		fields  fields
+		db      *sql.DB
 		args    args
 		want    int
 		wantErr bool
 	}{
 		{
 			name: "已在Book表中有记录的合法插入",
-			fields: fields{
-				DB: newTestPostgresql(),
-			},
+			db:   field.DB,
 			args: args{
 				ctx: context.Background(),
 				a:   &article.Article{},
@@ -83,14 +91,14 @@ func TestPostgresRepository_Insert(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			r := &PostgresRepository{
-				DB: tt.fields.DB,
+				DB: tt.db,
 			}
 			got, err := r.Insert(tt.args.ctx, tt.args.a)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("PostgresRepository.Insert() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
-			if got != tt.want {
+			if got < tt.want {
 				t.Errorf("PostgresRepository.Insert() = %v, want %v", got, tt.want)
 			}
 		})
