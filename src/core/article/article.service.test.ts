20718eaf30d521a577a024aea1443be3176fd477
zrecovery <zrecovery@outlook.com>
2023-11-03 22:45:11 +0800

    test: 增加测试

---
+++ src/core/article/article.service.test.ts
@@ -0,0 +1,95 @@
+import { expect, test, spyOn } from "bun:test";
+import ArticleService from "./article.service";
+import type { Query } from "./repository/ArticleRepository";
+import { ArticleMockRepository } from "@/infrastructure/mock/article.mock.repository";
+import type { Article } from "./model/article.model";
+
+const articleMockRepository = new ArticleMockRepository();
+const articleService = new ArticleService(articleMockRepository);
+
+test("通过id获取文章", async () => {
+  const article = await articleService.getById(1);
+  expect(article).toStrictEqual({
+    id: 1,
+    title: "测试标题",
+    book: "测试系列",
+    author: "测试作者",
+    serial_order: 1,
+    body: "测试内容",
+    love: false,
+  });
+});
+
+test("通过作者id获取文章", async () => {
+  const article = await articleService.getByAuthorId(1, 10, 0);
+  expect(article).toStrictEqual([
+    {
+      id: 1,
+      title: "测试标题",
+      book: "测试系列",
+      author: "测试作者1",
+      serial_order: 1,
+      body: "测试内容",
+      love: false,
+    },
+    {
+      id: 2,
+      title: "测试标题",
+      book: "测试系列",
+      author: "测试作者1",
+      serial_order: 1,
+      body: "测试内容",
+      love: false,
+    },
+  ]);
+});
+
+test("获取所有文章", async () => {
+  const query: Query = {
+    love: false,
+  };
+  const mockArticles: Article[] = [
+    {
+      id: 1,
+      title: "测试标题",
+      book: "测试系列",
+      author: "测试作者",
+      serial_order: 1,
+      body: "测试内容",
+      love: false,
+    },
+    {
+      id: 2,
+      title: "测试标题",
+      book: "测试系列",
+      author: "测试作者",
+      serial_order: 1,
+      body: "测试内容",
+      love: false,
+    },
+  ];
+  const spyFn = spyOn(articleMockRepository, "searchArticles");
+  spyFn.mockReturnValue(mockArticles);
+  const articles = await articleService.getList(query, 10, 0);
+  expect(spyFn).toHaveBeenCalled();
+  expect(articles).toStrictEqual([
+    {
+      id: 1,
+      title: "测试标题",
+      book: "测试系列",
+      author: "测试作者",
+      serial_order: 1,
+      body: "测试内容",
+      love: false,
+    },
+    {
+      id: 2,
+      title: "测试标题",
+      book: "测试系列",
+      author: "测试作者",
+      serial_order: 1,
+      body: "测试内容",
+      love: false,
+    },
+  ]);
+});
---
+++ src/infrastructure/mock/article.mock.repository.ts
@@ -0,0 +1,106 @@
+import type { Article } from "@/core/article/model/article.model";
+import type { ArticleRepository } from "@/core/article/repository/ArticleRepository";
+
+export class ArticleMockRepository implements ArticleRepository {
+  async getArticleById(id: number): Promise<Article> {
+    const article: Article = {
+      id,
+      title: "测试标题",
+      book: "测试系列",
+      author: "测试作者",
+      serial_order: 1,
+      body: "测试内容",
+      love: false,
+    };
+    return article;
+  }
+
+  async getArticlesByAuthorId(
+    authorId: number,
+    limit: number,
+    offset: number,
+  ): Promise<Article[]> {
+    return [
+      {
+        id: 1,
+        title: "测试标题",
+        book: "测试系列",
+        author: `测试作者${authorId}`,
+        serial_order: 1,
+        body: "测试内容",
+        love: false,
+      },
+      {
+        id: 2,
+        title: "测试标题",
+        book: "测试系列",
+        author: `测试作者${authorId}`,
+        serial_order: 1,
+        body: "测试内容",
+        love: false,
+      },
+    ];
+  }
+
+  async getArticles(limit: number, offset: number): Promise<Article[]> {
+    return [
+      {
+        id: 1,
+        title: "测试标题",
+        book: "测试系列",
+        author: "测试作者",
+        serial_order: 1,
+        body: "测试内容",
+        love: false,
+      },
+      {
+        id: 2,
+        title: "测试标题",
+        book: "测试系列",
+        author: "测试作者",
+        serial_order: 1,
+        body: "测试内容",
+        love: false,
+      },
+    ];
+  }
+
+  async createArticle(article: Article): Promise<void> {
+    throw new Error("Method not implemented.");
+  }
+
+  async updateArticle(article: Article): Promise<void> {
+    throw new Error("Method not implemented.");
+  }
+
+  async deleteArticle(id: number): Promise<void> {
+    throw new Error("Method not implemented.");
+  }
+
+  async searchArticles(
+    query: Query,
+    limit: number,
+    offset: number,
+  ): Promise<Article[]> {
+    return [
+      {
+        id: 1,
+        title: "测试标题",
+        book: "测试系列",
+        author: "测试作者",
+        serial_order: 1,
+        body: "测试内容",
+        love: false,
+      },
+      {
+        id: 2,
+        title: "测试标题",
+        book: "测试系列",
+        author: "测试作者",
+        serial_order: 1,
+        body: "测试内容",
+        love: false,
+      },
+    ];
+  }
+}
