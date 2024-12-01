7118e8c1a2f69e740f2a42378140631df14ca03e
zrecovery <zrecovery@outlook.com>
2024-12-01 15:22:52 +0800

    feat: 添加级联删除

--- apps/backend/script/create.sql
+++ apps/backend/script/create.sql
@@ -16,10 +16,10 @@ CREATE TABLE people (
 -- 关系表，表示文章作者
 CREATE TABLE authors (
   id SERIAL PRIMARY KEY,
-  created_at TIMESTAMP NOT NULL DEFAULT now(),
+  created_at TIMESTAMP NOT NULL DEFAULT now() ,
   updated_at TIMESTAMP NOT NULL DEFAULT now(),
-  person_id INTEGER REFERENCES people(id),
-  article_id INTEGER REFERENCES articles(id) unique
+  person_id INTEGER REFERENCES people(id) ON DELETE CASCADE,
+  article_id INTEGER REFERENCES articles(id) UNIQUE ON DELETE CASCADE
 );
 
 -- 表示系列
@@ -27,7 +27,7 @@ CREATE TABLE series (
   id SERIAL PRIMARY KEY,
   created_at TIMESTAMP NOT NULL DEFAULT now(),
   updated_at TIMESTAMP NOT NULL DEFAULT now(),
-  title TEXT NOT NULL unique
+  title TEXT NOT NULL UNIQUE
 );
 
 -- 表示文章在系列
@@ -35,10 +35,18 @@ CREATE TABLE chapters (
   id SERIAL PRIMARY KEY,
   created_at TIMESTAMP NOT NULL DEFAULT now(),
   updated_at TIMESTAMP NOT NULL DEFAULT now(),
-  article_id INTEGER REFERENCES articles(id) unique,
-  series_id INTEGER REFERENCES series(id),
+  article_id INTEGER REFERENCES articles(id) UNIQUE ON DELETE CASCADE,
+  series_id INTEGER REFERENCES series(id) ON DELETE CASCADE,
   "order" REAL NOT NULL DEFAULT 1.0
 );
+
+CREATE OR REPLACE FUNCTION update_timestamps()
+RETURNS TRIGGER AS $$
+BEGIN
+  NEW.updated_at = now();
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
 
 -- 添加所有表触发器，自动更新时间戳
 CREATE TRIGGER update_timestamps
@@ -46,26 +54,30 @@ CREATE TRIGGER update_timestamps
   ON articles
   FOR EACH ROW
 EXECUTE PROCEDURE update_timestamps();
+
 CREATE TRIGGER update_timestamps
   BEFORE UPDATE
   ON people
   FOR EACH ROW
 EXECUTE PROCEDURE update_timestamps();
+
 CREATE TRIGGER update_timestamps
   BEFORE UPDATE
   ON authors
   FOR EACH ROW
 EXECUTE PROCEDURE update_timestamps();
+
 CREATE TRIGGER update_timestamps
   BEFORE UPDATE
   ON series
   FOR EACH ROW
 EXECUTE PROCEDURE update_timestamps();
+
 CREATE TRIGGER update_timestamps
   BEFORE UPDATE
   ON chapters
   FOR EACH ROW
 EXECUTE PROCEDURE update_timestamps();
 
-create extension if not exists pgroonga;
+CREATE EXTENSION IF NOT EXISTS pgroonga;
 CREATE INDEX pgroonga_content_index ON articles USING pgroonga (title,body);
