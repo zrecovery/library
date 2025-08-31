921d42f3fb81c356aab57a879b7541d9098d9ed5
zrecovery <zrecovery@outlook.com>
2025-08-31 14:23:53 +0800

    chore: Disable SSR and remove prerender config

--- apps/web-client/app.config.ts
+++ apps/web-client/app.config.ts
@@ -2,13 +2,8 @@ import { defineConfig } from "@solidjs/start/config";
 import UnoCSS from "unocss/vite";
 
 export default defineConfig({
+  ssr: false,
   vite: {
     plugins: [UnoCSS()],
-  },
-  server: {
-    prerender: {
-      crawlLinks: true
-    },
-
   }
 });
