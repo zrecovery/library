b4b9eac2122526a2c118b13180c7556da18e076e
zrecovery <zrecovery@outlook.com>
2026-04-02 21:44:34 +0800

    feat: Add Elysia HTTP service for article creation

---
+++ packages/usecase/src/modules/articles/create/create.elysia.ts
@@ -0,0 +1,29 @@
+import { Elysia, status } from "elysia";
+import { CreateArticleUseCase } from "./create-article.usecase";
+import { ArticleCreatePort } from "./port/type/create";
+import { Id } from "@library/domain/common";
+import { ElysiaType } from "elysia/type-system";
+
+export const createArticleHttpService = (
+  articleCreate: CreateArticleUseCase,
+) => {
+  return new Elysia().post(
+    "/",
+    async ({ body }) => {
+      const result = await articleCreate.execute(body);
+      if (result.isOk()) {
+        return result.unwrap();
+      } else {
+        result.unwrapErr();
+        return status(500, "未知错误");
+      }
+    },
+    {
+      body: ArticleCreatePort,
+      response: {
+        200: Id,
+        500: ElysiaType.String(),
+      },
+    },
+  );
+};
