8c1bc0b9a1fd580fe4c55079d1486c70e8b6f6e6
zrecovery <zrecovery@outlook.com>
2026-04-12 20:39:46 +0800

    fix: Add backend app using Elysia and Bun

---
+++ apps/backend/index.ts
@@ -0,0 +1,15 @@
+import { Elysia } from "elysia";
+import { articleHttpService } from "@library/usecase";
+import { drizzle } from "drizzle-orm/bun-sqlite";
+import { Database } from "bun:sqlite";
+import { env } from "bun";
+const uri = env.DATABASE_URI;
+const db = new Database(uri, { create: true });
+const client = drizzle(db, {
+  logger: true,
+});
+
+const app = new Elysia({ prefix: "/api" })
+  .use(articleHttpService(client))
+  .listen(3001);
+export type App = typeof app;
